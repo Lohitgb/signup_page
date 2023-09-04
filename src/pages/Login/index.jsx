8dfca1810069f96c0632e1e9a2aa7551ed509b3e
Lohit GB <lgb@patgardeepa95@gmail.com>
2023-09-04 18:02:28 +0530

    css updated

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,35 +1,30 @@
-import '@/pages/Login/index.css';
+import "@/pages/Login/index.css";
 function Login() {
-    return (
-      <div className="container">
-
-        <div className="section1">
-            <div className="title">
-              <h2>Login</h2>
-              <p>Log in and start creating your next task</p>
-              <p>
-                Do not have an account?
-                <a href=""> Sign up</a>
-              </p>
-              </div>
-        </div>
-        
-  
-        <div className="section2">
-          <form>
-              <input placeholder="Email" type="email" required />
-              <input placeholder="Password" type="password" />
-            
-            <div className="login_section">
-              <button type="submit">Login</button>
-              <p className="forgot">
-                Forgot your password ?
-                <a href="">Forgot Password</a>
-              </p>
-            </div>
-          </form>
+  return (
+    <div className="container">
+      <div className="section1">
+        <div className="title">
+          <h2>Login</h2>
+          <p>Log in and start creating your next task</p>
+          <p>
+            Do not have an account ?<a href="#"> Sign up</a>
+          </p>
         </div>
       </div>
-    );
-  }
+
+      <div className="section2">
+        <form>
+          <input placeholder="Email" type="email" required />
+          <input placeholder="Password" type="password" />
+          <div className="login_section">
+            <button type="submit">Login</button>
+            <p className="forgot">
+              Forgot your password ?<a href="#"> Forgot Password</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 export default Login;
