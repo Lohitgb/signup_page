6f655dca4327d0c9f9a0bd2309f19d8d8825671d
Lohit GB <lgb@patgardeepa95@gmail.com>
2023-09-12 11:05:31 +0530

    css and html updated

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -5,28 +5,29 @@ function Signup() {
       <div className="signup_container">
         <div className="signup_section1">
           <form>
-          <div className="input-box">
-          <input placeholder="Username" type="text" required />
-          <input placeholder="Email" type="email" required />
-          <input placeholder="Password" type="password" required />
-          <input placeholder="Confirm Password" type="password" required />
-          </div>
+            <input placeholder="Username" type="text" required />
+            <input placeholder="Email" type="email" required />
+            <input placeholder="Password" type="password" required />
+            <input placeholder="Password Confirm" type="password" required />
 
-          <div className="signup_section">
-            <button type="submit">Sign Up</button>
-              </div> 
+            <div className="signup_section">
+              <button type="submit">Sign Up</button>
+            </div>
           </form>
         </div>
 
-          <div className="signup_section2">
-            <div className="signup_tittle">
-              <h2>Sign Up</h2>
-              <p>Register and create an account on Todo List. Write your tasks anytime and anywhere</p>
-              <p>
+        <div className="signup_section2">
+          <div className="signup_tittle">
+            <h2>Sign Up</h2>
+            <p>
+              Register and create an account on Todo List. Write your tasks
+              anytime and anywhere
+            </p>
+            <p>
               Already have an account ? <a href="#"> Login </a>
-              </p>
-            </div>
+            </p>
           </div>
+        </div>
       </div>
     </div>
   );
