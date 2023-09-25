557963cb4660ef7613ab853520ce03db860b1b9f
Lohit GB <lgb@patgardeepa95@gmail.com>
2023-09-25 17:36:13 +0530

    forgot password responsive done

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -1,36 +1,35 @@
-import About from './About';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
-import ForgotPassword from './ForgotPassword';
+import About from "./About";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import ForgotPassword from "./ForgotPassword";
 
 const pages = [
   {
-    path: '',
+    path: "",
     element: <Home />,
-    title: 'home',
+    title: "home",
   },
   {
-    path: 'about',
+    path: "about",
     element: <About />,
-    title: 'about',
+    title: "about",
   },
   {
-  path:'login',
-  element:<Login/>,
-  title:'login'
+    path: "login",
+    element: <Login />,
+    title: "login",
   },
   {
-    path:'signup',
-    element:<Signup/>,
-    title:'signup',
+    path: "signup",
+    element: <Signup />,
+    title: "signup",
   },
   {
-    path:'forgot-password',
-    element:<ForgotPassword/>,
-    title:'forgot-password'
+    path: "forgot-password",
+    element: <ForgotPassword />,
+    title: "forgot-password",
   },
-
 ];
 
 export default pages;
